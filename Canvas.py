fe4a3342b25bae0c73936f0e6b2b62be641f979a
Mohaiu Teodora <mohaiuantoniateodora@yahoo.com>
2021-04-17 23:24:33 +0300

    Added more canvases for matplotlib figures.

--- Canvas.py
+++ Canvas.py
@@ -1,6 +1,8 @@
 from matplotlib.figure import Figure
 from matplotlib.backends.backend_qt5agg import FigureCanvasQTAgg as FigureCanvas
 from matplotlib.backend_bases import NavigationToolbar2
+import matplotlib.pyplot as plt
+import seaborn as sns
 
 
 class MplCanvas(FigureCanvas):
@@ -12,5 +14,18 @@ class MplCanvas(FigureCanvas):
         super(MplCanvas, self).__init__(fig)
 
 
+class MultipleSubplotsCanvas(FigureCanvas):
+    def __init__(self, parent=None, width=5, height=4, dpi=100):
+        fig = Figure(figsize=(width, height), dpi=dpi)
+        fig.tight_layout()
+        fig.patch.set_facecolor("black")
+        self.ax1 = fig.add_subplot(121)
+        self.ax2 = fig.add_subplot(122)
+        super(MultipleSubplotsCanvas, self).__init__(fig)
 
+
+class SeabornCanvas(FigureCanvas):
+    def __init__(self, parent=None, width=5, height=4, dpi=100):
+        fig, self.ax = plt.subplots(figsize=(width, height))
+        super(SeabornCanvas, self).__init__(fig)
 
